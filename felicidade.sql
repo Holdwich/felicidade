ca253c17484897ff12878ebe1a07aecbbe8e5c8f
gioheex <81923318+gioheex@users.noreply.github.com>
2023-11-03 17:40:41 -0300

    Add files via upload
    maior consistência de nomes de tabelas e remoção de localização na tabela tb_ocorrência.

--- felicidade.sql
+++ felicidade.sql
@@ -2,7 +2,7 @@ CREATE database felicidade;
 
 use felicidade;
 
-CREATE TABLE pessoa (
+CREATE TABLE tb_pessoa (
     id_pessoa INT AUTO_INCREMENT PRIMARY KEY,
     permissao BOOLEAN NOT NULL,
     CPF varchar(11) NOT NULL UNIQUE,
@@ -15,48 +15,47 @@ CREATE TABLE pessoa (
     CEP varchar(8) NOT NULL
 );
 
-CREATE TABLE setor (
+CREATE TABLE tb_setor (
     id_setor INT AUTO_INCREMENT PRIMARY KEY,
     setor_nome varchar(45) NOT NULL UNIQUE
 );
 
-CREATE TABLE tipo_ocorrencia (
+CREATE TABLE tb_tipo_ocorrencia (
     id_tipo_ocorrencia INT AUTO_INCREMENT PRIMARY KEY,
-    id_setor_fk INT, 
-    tipo_ocorrencia_tipo VARCHAR(30) NOT NULL UNIQUE,
+    id_setor_fk INT NOT NULL, 
+    tipo_ocorrencia_nome VARCHAR(30) NOT NULL UNIQUE,
     FOREIGN KEY (id_setor_fk) REFERENCES setor(id_setor)
 );
 
-CREATE TABLE lugar (
+CREATE TABLE tb_lugar (
     id_lugar INT AUTO_INCREMENT PRIMARY KEY,
     lugar_nome VARCHAR(90)
 );
 
-CREATE TABLE sub_lugar (
+CREATE TABLE tb_sub_lugar (
     id_sub_lugar INT AUTO_INCREMENT PRIMARY KEY,
-    id_lugar_fk INT,
-    sub_local_nome VARCHAR(90),
+    id_lugar_fk INT NOT NULL,
+    sub_lugar_nome VARCHAR(90),
     foreign key (id_lugar_fk) REFERENCES lugar (id_lugar)
 );
 
-CREATE TABLE ocorrencia (
+CREATE TABLE tb_ocorrencia (
     id_ocorrencia INT AUTO_INCREMENT PRIMARY KEY,
-    id_tipo_ocorrencia_fk int,
-    id_pessoa_fk int,
-    id_sub_lugar_fk int,
+    id_tipo_ocorrencia_fk int NOT NULL,
+    id_pessoa_fk int NOT NULL,
+    id_sub_lugar_fk int NOT NULL,
     ocorrencia_descricao TEXT NOT NULL,
-    data_ocorrencia DATE NOT NULL,
-    localizacao VARCHAR(100) NOT NULL,
-    data_registro datetime NOT NULL,
+    ocorrencia_data DATE NOT NULL,
+    ocorrencia_data_registro datetime NOT NULL,
     ocorrencia_status BOOLEAN NOT NULL,
     FOREIGN KEY (id_tipo_ocorrencia_fk) REFERENCES tipo_ocorrencia (id_tipo_ocorrencia),
     foreign key (id_sub_lugar_fk) REFERENCES sub_lugar (id_sub_lugar),
     FOREIGN KEY (id_pessoa_fk) REFERENCES pessoa(id_pessoa)
 );
 
-CREATE TABLE midia (
+CREATE TABLE tb_midia (
     id_midia INT AUTO_INCREMENT PRIMARY KEY,
-    id_ocorrencia_fk INT,
+    id_ocorrencia_fk INT NOT NULL,
     midia_descrição text NOT NULL,
     midia longblob,
     FOREIGN KEY (id_ocorrencia_fk) REFERENCES ocorrencia(id_ocorrencia)
