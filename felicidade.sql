59f2580fa80f5b51b797ee620ad568313190a438
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2023-11-07 21:08:47 -0300

    Update felicidade.sql

--- felicidade.sql
+++ felicidade.sql
@@ -24,19 +24,19 @@ CREATE TABLE tb_tipo_ocorrencia (
     id_tipo_ocorrencia INT AUTO_INCREMENT PRIMARY KEY,
     id_setor_fk INT NOT NULL, 
     tipo_ocorrencia_nome VARCHAR(30) NOT NULL UNIQUE,
-    FOREIGN KEY (id_setor_fk) REFERENCES setor(id_setor)
+    FOREIGN KEY (id_setor_fk) REFERENCES tb_setor(id_setor)
 );
 
 CREATE TABLE tb_lugar (
     id_lugar INT AUTO_INCREMENT PRIMARY KEY,
-    lugar_nome VARCHAR(90)
+    lugar_nome VARCHAR(90) NOT NULL UNIQUE
 );
 
 CREATE TABLE tb_sub_lugar (
     id_sub_lugar INT AUTO_INCREMENT PRIMARY KEY,
     id_lugar_fk INT NOT NULL,
     sub_lugar_nome VARCHAR(90),
-    foreign key (id_lugar_fk) REFERENCES lugar (id_lugar)
+    foreign key (id_lugar_fk) REFERENCES tb_lugar (id_lugar)
 );
 
 CREATE TABLE tb_ocorrencia (
@@ -48,9 +48,9 @@ CREATE TABLE tb_ocorrencia (
     ocorrencia_data DATE NOT NULL,
     ocorrencia_data_registro datetime NOT NULL,
     ocorrencia_status BOOLEAN NOT NULL,
-    FOREIGN KEY (id_tipo_ocorrencia_fk) REFERENCES tipo_ocorrencia (id_tipo_ocorrencia),
-    foreign key (id_sub_lugar_fk) REFERENCES sub_lugar (id_sub_lugar),
-    FOREIGN KEY (id_pessoa_fk) REFERENCES pessoa(id_pessoa)
+    FOREIGN KEY (id_tipo_ocorrencia_fk) REFERENCES tb_tipo_ocorrencia (id_tipo_ocorrencia),
+    foreign key (id_sub_lugar_fk) REFERENCES tb_sub_lugar (id_sub_lugar),
+    FOREIGN KEY (id_pessoa_fk) REFERENCES tb_pessoa(id_pessoa)
 );
 
 CREATE TABLE tb_midia (
@@ -58,5 +58,5 @@ CREATE TABLE tb_midia (
     id_ocorrencia_fk INT,
     midia_descrição text,
     midia longblob,
-    FOREIGN KEY (id_ocorrencia_fk) REFERENCES ocorrencia(id_ocorrencia)
+    FOREIGN KEY (id_ocorrencia_fk) REFERENCES tb_ocorrencia(id_ocorrencia)
 );
