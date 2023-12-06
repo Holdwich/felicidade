e0dadc5776df654571a6c4eaadba1a3026af880d
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2023-12-05 22:09:40 -0300

    Update DAO.py

--- DAO.py
+++ DAO.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text , func
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 
